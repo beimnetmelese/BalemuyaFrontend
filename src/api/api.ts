6ad973bb7e66987986953d95d1f5bb8d2894203a
beimnetmelese <beimnetmelese16@gmail.com>
2026-02-12 23:48:02 +0300

    bug fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,9 @@
 // Utility to get Telegram ID from WebApp
-export function getTelegramId(): string | null {
-  try {
-    // @ts-ignore
-    if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initDataUnsafe && window.Telegram.WebApp.initDataUnsafe.user) {
-      // @ts-ignore
-      return window.Telegram.WebApp.initDataUnsafe.user.id?.toString() || null;
-    }
-  } catch (e) {}
-  return null;
-}
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://balemuyabackend.onrender.com/api', // Change to your Django backend URL
+  baseURL: 'https://aguero.pythonanywhere.com/api', // Change to your Django backend URL
   headers: {
     'Content-Type': 'application/json',
 
